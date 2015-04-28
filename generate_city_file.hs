8919164933f3d692c1222fcf8bf16fe87613f6c2
J. David Giese <johndgiese@gmail.com>
2015-04-28 12:08:22 -0500

    Fix up the solution

--- generate_city_file.hs
+++ generate_city_file.hs
@@ -1,32 +1,31 @@
 import Data.Char
-import Math
 
 
 data Point = Point Float Float
-data Vertex = Vertex { name :: String, location :: Point }
+data Vertex = Vertex Int Point
 
 
 readVertex :: Int -> String -> Vertex
-readVertex n line = Vertex {name=name, location=point}
-    where name = show n
-          coordinates = map (read :: Float) (words line)
-          x = coordinates !! 0
-          y = coordinates !! 1
-          point = Point x y
+readVertex name line = Vertex name location
+    where [x, y] = map read $ words line
+          location = Point x y
 
 distance :: Point -> Point -> Float
-distance (Point px py) (Point qx qy) = Math.sqrt((px - qx)*(px - qx) + (py - qy)*(py - qy))
+distance (Point px py) (Point qx qy) = sqrt((px - qx)*(px - qx) + (py - qy)*(py - qy))
 
 weight :: Vertex -> Vertex -> Float
-weight v1 v2 = distance v1.location v2.location
+weight (Vertex _ l1) (Vertex _ l2) = distance l1 l2
 
-shortestPath :: [Vertex] -> [String]
-shortestPath _ = ["1", "2", "3"]  -- TODO: implement
+shortestPath :: [Vertex] -> [Int]
+shortestPath _ = [1, 2, 3]  -- TODO: implement
 
 
+-- Read in a list of city locations from standard input (each line should have
+-- two space-separated floating point numbers).  Print out the solution to the
+-- traveling salesperson problem on standard input, assuming you start at the
+-- city on the first line of standard input.  The solution is a series of
+-- integers (cooresponding to the line number on standard input) that the
+-- salesperson is passing through.
 main = do
     contents <- getContents
-    print $ unlines path
-    where
-        vertices = zipWith readVertex [1..] (lines contents)
-        path = shortestPath vertices
+    mapM_ print $ shortestPath $ zipWith readVertex [1..] (lines contents)
